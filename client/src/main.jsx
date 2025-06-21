eff69367f58d8bded2915ef96ed144c998e27c88
Aneesh Kolar <kolaraneesh2003@gmail.com>
2025-06-21 20:37:05 +0530

    Added all other components and hooks

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "stream-chat-react/dist/css/v2/index.css";
 import './index.css'
 import App from './App.jsx'
 
@@ -20,4 +21,4 @@ createRoot(document.getElementById('root')).render(
     </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
